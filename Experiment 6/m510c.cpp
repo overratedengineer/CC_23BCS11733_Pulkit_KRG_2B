13d26548cf1e55af05ea74200556cd09d7376273
Pulkit <choprapulkitt@gmail.com>
2026-03-25 14:03:59 +0530

    Add files via upload

---
+++ Experiment 6/m510c.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<string> names(n);
+    for (int i = 0; i < n; i++) {
+        cin >> names[i];
+    }
+
+    vector<vector<int>> adj(26);
+    vector<int> indegree(26, 0);
+
+    for (int i = 0; i < n - 1; i++) {
+        string s = names[i];
+        string t = names[i + 1];
+
+        int len = min(s.size(), t.size());
+        bool found = false;
+
+        for (int j = 0; j < len; j++) {
+            if (s[j] != t[j]) {
+                adj[s[j] - 'a'].push_back(t[j] - 'a');
+                indegree[t[j] - 'a']++;
+                found = true;
+                break;
+            }
+        }
+
+        if (!found && s.size() > t.size()) {
+            cout << "Impossible\n";
+            return 0;
+        }
+    }
+
+    queue<int> q;
+    for (int i = 0; i < 26; i++) {
+        if (indegree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    string result = "";
+
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        result += (char)(u + 'a');
+
+        for (auto v : adj[u]) {
+            indegree[v]--;
+            if (indegree[v] == 0) {
+                q.push(v);
+            }
+        }
+    }
+
+
+    if (result.size() < 26) {
+        cout << "Impossible\n";
+    } else {
+        cout << result << "\n";
+    }
+
+    return 0;
+}
